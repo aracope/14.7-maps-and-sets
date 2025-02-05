8cc4df2acc1688d514fedf5979664edcd369666b
aracope <156398622+aracope@users.noreply.github.com>
2025-02-04 23:50:43 -0800

    Complete hasDuplicate function.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Quick Question #1
 // What does the following code return?
-new Set([1,1,2,2,3,4])
-//ANSWER: Set(4) {1, 2, 3, 4}
+// new Set([1,1,2,2,3,4])
+// ANSWER: Set(4) {1, 2, 3, 4}
 
 // Quick Question #2
 // What does the following code return?
@@ -10,18 +10,27 @@ new Set([1,1,2,2,3,4])
 
 // Quick Questions #3
 // What does the Map m look like after running the following code?
-let m = new Map();
-m.set([1,2,3], true);
-m.set([1,2,3], false);
-//ANSWER: Map(2) {Array(3) => true, Array(3) => false}
+// let m = new Map();
+// m.set([1,2,3], true);
+// m.set([1,2,3], false);
+// ANSWER: Map(2) {Array(3) => true, Array(3) => false}
 
 // hasDuplicate
 // Write a function called hasDuplicate which accepts an array and returns true or false if that array contains a duplicate
 // hasDuplicate([1,3,2,1]) // true
 // hasDuplicate([1,5,-1,4]) // false
 
+function hasDuplicate(array) {
+  return new Set(array).size !== array.length;
+}
+
+console.log(hasDuplicate([1,3,2,1])); // true
+console.log(hasDuplicate([1,5,-1,4])); // false
 
 // vowelCount
 // Write a function called vowelCount which accepts a string and returns a map where the keys are numbers and the values are the count of the vowels in the string.
 // vowelCount('awesome') // Map { 'a' => 1, 'e' => 2, 'o' => 1 }
 // vowelCount('Colt') // Map { 'o' => 1 }
+
+console.log(vowelCount('awesome')); // Map { 'a' => 1, 'e' => 2, 'o' => 1 }
+console.loge(vowelCount('Colt')); // Map { 'o' => 1 }
